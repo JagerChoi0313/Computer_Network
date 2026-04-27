5dbb82e99e9ac73c52c86ea3f0252dbeed240de3
JagerChoi <17322905680@qq.com>
2026-04-27 19:21:54 +0800

    TCP通信：服务端开发

--- src/ChoiLearnNetwork4/Server.java
+++ src/ChoiLearnNetwork4/Server.java
@@ -1,4 +1,34 @@
 package ChoiLearnNetwork4;
 
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) throws Exception{
+        System.out.println("服务端启动成功");
+
+        //1,创建ServerSocket的对象，，同时为服务端注册端口
+        ServerSocket serverSocket = new ServerSocket(8888);
+
+        //2,使用ServerSocket对象，调用一个accept方法，等待客户端的连接请求
+        Socket socket = serverSocket.accept();
+
+        //3,从Socket的通信管道中得到一个字节输入流
+        InputStream is = socket.getInputStream();
+
+        //4,把原始的字节输入流包装成数据输入流
+        DataInputStream dis = new DataInputStream(is);
+
+        //5,使用数据输入流读取客户端发送来的消息
+        String rs = dis.readUTF();
+        System.out.println(rs);
+
+        //其实我们也可以获得客户端的IP地址
+        System.out.println(socket.getRemoteSocketAddress());
+
+        dis.close();
+        socket.close();
+    }
 }
